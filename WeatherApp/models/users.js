914dade6dfd8b6082805f1378ceaa0857a98d7a2
Gonzalo Punta Perez <gpuntaperez@safareyes.es>
2019-12-16 19:05:14 +0100

    Users model

--- WeatherApp/models/users.js
+++ WeatherApp/models/users.js
@@ -1,1 +1,19 @@
 //Crear modelo usuario
+'use strict'
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    fullname: String,
+    creationDate: Date,
+    username: String,
+    password: String,
+    email: String,
+    roles: String,
+    registered_station: {type: mongoose.Schema.Types.ObjectId},
+    keep_station: {type: mongoose.Schema.Types.ObjectId},
+    rol: String
+});
+
+
+module.exports = mongoose.model('User', userSchema);
