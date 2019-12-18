48032da72019b4cb12b582856ca103f464739704
Lucas Amado <amadolucas99@gmail.com>
2019-12-18 14:02:57 +0100

    find stattion by id y creación .gitignore

--- WeatherApp/routes/stations.js
+++ WeatherApp/routes/stations.js
@@ -4,8 +4,9 @@ const express = require('express')
 const router = express.Router()
 const middleware = require('../middleware/index');
 const EstacionController = require('../controllers/station')
+const contoller = require('../controllers/station-lucas')
 
-
+router.get('/:id', middleware.ensureAuthenticatedManager, contoller.getById);
 //router.get('/', middleware.ensureAuthenticated, EstacionController.getTodos);
 //router.get('/', EstacionController.getTodos);
 
