6a469f07a36bc400542530c79f48741adbcb9adb
Esperanza Macarena <espemacem@gmail.com>
2019-12-19 13:07:58 +0100

    conflicto rutas

--- WeatherApp/routes/weathers.js
+++ WeatherApp/routes/weathers.js
@@ -8,12 +8,13 @@ const WeatherController = require('../controllers/weather')
 
 
 //router.get('/', middleware.ensureAuthenticated, WeatherController.getTodos);
+router.get('/today',middleware.ensureAuthenticated,controller.getWeather);
+router.get('/from/:from/to/:to',middleware.ensureAuthenticated,controller.getWeather);
+
 router.get('/:id', WeatherController.getUno);
 
 //router.post('/', middleware.ensureAuthenticated, WeatherController.nuevaWeather);
 router.post('/', WeatherController.nuevoWeather);
 
-router.get('/today',middleware.ensureAuthenticated,controller.getWeather);
-router.get('/from/:from/to/:to',middleware.ensureAuthenticated,controller.getWeather);
 
 module.exports = router
